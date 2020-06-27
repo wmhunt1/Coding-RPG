c43e5c4793d0f23820680bd0d9282945cb98d759
Matt Hunt <wmatthunt@gmail.com>
2020-06-27 13:33:29 -0400

    moved js to seperate file

---
+++ Scripts.js
@@ -0,0 +1,67 @@
+var Player = 
+        {
+            Name: "Character",
+            Level: 1,
+            CurrentHP: 10,
+            MaxHP: 10,
+            Gold: 0,
+            Atk: 2,
+            Armor: 1,
+            Dead: false,
+            Magic: false,
+        }
+        // Need to figure out how to fill this for each enemy or make it a template
+        var Enemy = 
+        {
+            Name: "N",
+            CurrentHP: 5,
+            MaxHP: 5,
+            Atk: 2,
+            Armor: 0,
+            Dead: false,
+        }
+    // functions
+        function DMGPlayer()
+        {
+            //Should reduce damage by armor amount
+            Player.CurrentHP = Player.CurrentHP - (Enemy.Atk - Player.Armor);
+            //should kill player when HP is reduced to 0
+            /*When (Player.CurrentHP <= 0)
+            {
+                Player.Dead = true;
+            }*/
+        }
+        function DMGEnemy()
+        {
+            //should reduce damage by armor amount
+            Enemy.CurrentHP = Enemy.CurrentHP - (Player.Atk - Enemy.Armor);
+           /* When (Enemy.CurrentHP <= 0)
+            {
+                Enemy.Dead = true;
+            }*/
+        }
+        function LevelUp()
+        {
+            Player.Level += 1;
+            MaxHP += 10;
+        }
+        function AddGold(x)
+        {
+            Player.Gold += x;
+        }
+        function RemoveGold(x)
+        {
+            Player.Gold -= x;
+        }
+        function Rest()
+        {
+            Player.CurrentHP = Player.MaxHP;
+        }
+        function UseItem()
+        {
+            //Need to make items
+        }
+        DMGEnemy()
+        console.log(Enemy.CurrentHP)
+        DMGPlayer()
+        console.log(Player.CurrentHP)
