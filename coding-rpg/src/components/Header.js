47fdb9eaab5cfade3c3268a779594996e94b9abc
Matt Hunt <wmatthunt@gmail.com>
2020-10-20 14:19:46 -0400

    working on header

---
+++ coding-rpg/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return (
+    //   <header style={styles.headerStyle} className="header">
+    //     <h1 style={styles.headingStyle}>Welcome</h1>
+    //   </header>
+    <header>Coding RPG</header>
+    );
+  }
+  
+  export default Header;
