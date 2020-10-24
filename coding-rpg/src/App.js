c4921b457013f10cf68eb53788efdd2bc074a5d1
Matt Hunt <wmatthunt@gmail.com>
2020-10-24 11:15:20 -0400

    got rid of stuff

--- coding-rpg/src/App.js
+++ coding-rpg/src/App.js
@@ -1,33 +1,11 @@
 import React from "react";
 import Header from "./components/Header";
-import Menu from "./components/Menu";
-import Intro from "./components/Intro";
-import CharCreate from "./components/CharCreate";
 
-function Start() {
+function Game() {
     return (
         <div>
             <Header/>
-            {/* <Menu/> */}
-            <Intro/>
         </div>
     );
 }
-function CreateCharacter() {
-    return (
-        <div>
-            <Header/>
-            <CharCreate/>
-        </div>
-    );
-}
-function MainGame() {
-    return (
-        <div>
-            <Header/>
-            <Menu/>
-        </div>
-    );
-}
-
-export default Start;
+export default Game;
--- coding-rpg/src/components/CharCreate.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function CharCreate() {
-  return (
-    <h1>Character Creation</h1>
-  );
-}
-
-export default CharCreate;
