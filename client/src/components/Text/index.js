c5305334001909e9ce4e85e33e556586270618d0
Matt Hunt <wmatthunt@gmail.com>
2020-12-14 15:14:38 -0500

    adding consts

--- client/src/components/Text/index.js
+++ client/src/components/Text/index.js
@@ -1,8 +1,9 @@
 import React, { useState, } from 'react';
 
 export default function Text() {
-    const [job, setJob] = useState("Freelancer");
-    const [race, setRace] = useState("None")
+    const [name, setName] = useState("Name");
+    const [race, setRace] = useState("Race");
+    const [job, setJob] = useState("Class");
     const story = [
         {
             storyText: 'Welcome to Coding RPG',
@@ -19,26 +20,26 @@ export default function Text() {
         {
             storyText: 'What is your name?',
             answerOptions: [
-                { answerText: "Hero"},
-            ]
-        },
-        {
-            storyText: 'Choose your Class.',
-            answerOptions: [
-                { answerText: "Fighter"},
-                { answerText: "Rogue" },
-                { answerText: "Wizard"},
+                { answerText: "Hero" },
+                { answerText: "Joe" }
             ]
         },
         {
             storyText: 'Choose your Race',
             answerOptions: [
-                { answerText: "Human"},
-                { answerText: "Dwarf"},
-                { answerText: "Elf"},
+                { answerText: "Human" },
+                { answerText: "Dwarf" },
+                { answerText: "Elf" },
             ]
         },
-
+        {
+            storyText: 'Choose your Class.',
+            answerOptions: [
+                { answerText: "Fighter" },
+                { answerText: "Rogue" },
+                { answerText: "Wizard" },
+            ]
+        }
     ]
     const [gameOver, setGameOver] = useState(false);
     const [currentText, setCurrentText] = useState(0);
@@ -63,8 +64,9 @@ export default function Text() {
             ) : (
                     <>
                         <div className='story-section'>
-                            <h1>{job}</h1>
+                            <h1>{name}</h1>
                             <h1>{race}</h1>
+                            <h1>{job}</h1>
                             <div className='story-text'>{story[currentText].storyText}</div>
                         </div>
                         <div className='answer-section'>
