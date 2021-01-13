66002164320f34043957eace5b33aa5402514475
Matt Hunt <wmatthunt@gmail.com>
2021-01-12 19:13:42 -0500

    minor work on combat

--- client/src/components/Combat/index.js
+++ client/src/components/Combat/index.js
@@ -1,8 +1,9 @@
 import React, { useState, } from 'react';
+import "./index.css"
 import Goblin from "../../assets/characters/goblin.png";
-//import Player from "../../assets/characters/player.png";
-let hero = { name: "Hero", img: "" };
-let goblin = { name: "Goblin", img: Goblin };
+import Player from "../../assets/characters/player.png";
+let hero = { name: "Hero", img: Player, hp: 10 };
+let goblin = { name: "Goblin", img: Goblin, hp: 10 };
 export default function Combat() {
     const [enemy, setEnemy] = useState(goblin);
     const [player, setPlayer] = useState(hero);
@@ -10,12 +11,14 @@ export default function Combat() {
         <div id="combat-window">
             <h3>Combat</h3>
             <div id="enemy-div">
-                {enemy.name}
-                <img src={enemy.img} alt=""></img>
+                <h4>Name: {enemy.name}</h4>
+                <img height="100px" width="100px" src={enemy.img} alt=""></img>
+                <p>HP: {enemy.hp}</p>
             </div>
             <div id="player-div">
-                {player.name}
-                <img src={player.img} alt=""></img>
+                <h4>Name: {player.name}</h4>
+                <img height="100px" width="100px" src={player.img} alt=""></img>
+                <p>HP: {player.hp}</p>
             </div>
         </div>
     )
