98ff7627dc7b4b7c5e831071aff3ff5095afa0db
wmhunt1 <wmatthunt@gmail.com>
2023-02-08 14:46:29 -0500

    fixed inventory screen

--- coding-rpg-react/src/Components/Inventory.js
+++ coding-rpg-react/src/Components/Inventory.js
@@ -38,13 +38,13 @@ function Inventory(props) {
     props.parentCallback(hero);
   }
   const charList = allies.map((ally, index) => <h4 key={index}>{ally.Name} <button onClick={() => setActiveUser(ally)}><h4>Set Active Character</h4></button></h4>)
-  const itemList = inventory.sort((a, b) => a.Name.localeCompare(b.Name)).map((item, index) => <h4 key={index}>{item.Name} - Price: {item.Cost} GP, QTY: {item.Quantity} <button onClick={() => { handleItem(activeUser, hero.Inventory, item) }}>{item.Type === "Equipable" ? <h4>Equip</h4> : <h4>Inspect</h4>}{item.Type === "Consumable" ? <h4>Use</h4> : <h4>Inspect</h4>}</button></h4>)
+  const itemList = inventory.sort((a, b) => a.Name.localeCompare(b.Name)).map((item, index) => <h4 key={index}>{item.Name} - Price: {item.Cost} GP, QTY: {item.Quantity} {item.Type === "Equipable" ? <button onClick={() => { handleItem(activeUser, hero.Inventory, item) }}> Equip </button> : <div></div>}{item.Type === "Consumable" ? <button onClick={() => { handleItem(activeUser, hero.Inventory, item) }}> Consume </button> : <div></div>}</h4>)
   const consumeList = inventory.sort((a, b) => a.Name.localeCompare(b.Name)).filter(item => item.Type === "Consumable").map((item, index) => <h4 key={index}>{item.Name} - Price: {item.Cost} GP, QTY: {item.Quantity} <button onClick={() => { handleItem(activeUser, hero.Inventory, item) }}>{item.Type === "Equipable" ? <h4>Equip</h4> : <h4></h4>}{item.Type === "Consumable" ? <h4>Use</h4> : <h4></h4>}</button></h4>)
   const equipList = inventory.sort((a, b) => a.Name.localeCompare(b.Name)).filter(item => item.Type === "Equipable").map((item, index) => <h4 key={index}>{item.Name} - Price: {item.Cost} GP, QTY: {item.Quantity} <button onClick={() => { handleItem(activeUser, hero.Inventory, item) }}>{item.Type === "Equipable" ? <h4>Equip</h4> : <h4></h4>}{item.Type === "Consumable" ? <h4>Use</h4> : <h4></h4>}</button></h4>)
   return (<div>
     <div>
       <h2>{hero.Name}'s Inventory</h2>
-      <h3>{hero.Gold} GP</h3>
+      <h3>{hero.Name} has {hero.Gold} GP</h3>
       <div className='inv-box'>
         <h4>Active User: {activeUser.Name}</h4>
         {charList}
@@ -52,9 +52,9 @@ function Inventory(props) {
       <div className='inv-box'>
         <h4>Items</h4>
         <h5>Item Filters: <button onClick={() => setActiveList("Consumable")}>Consumables</button> <button onClick={() => setActiveList("Equipable")}>Equipable</button> <button onClick={() => setActiveList("Default")}>Clear</button></h5>
-        {activeList === "Default" ? <div>{itemList.length > 0 ? <div>{itemList}</div> : <div><h4>Inventory is Empty</h4></div>}</div>:<div></div>}
-        {activeList === "Consumable" ? <div>{consumeList.length > 0 ? <div>{consumeList}</div> : <div><h4>No Consumables Items</h4></div>}</div>:<div></div>}
-        {activeList === "Equipable" ? <div>{equipList.length > 0 ? <div>{equipList}</div> : <div><h4>No Equipable Items</h4></div>}</div>:<div></div>}
+        {activeList === "Default" ? <div>{itemList.length > 0 ? <div>{itemList}</div> : <div><h4>Inventory is Empty</h4></div>}</div> : <div></div>}
+        {activeList === "Consumable" ? <div>{consumeList.length > 0 ? <div>{consumeList}</div> : <div><h4>No Consumables Items</h4></div>}</div> : <div></div>}
+        {activeList === "Equipable" ? <div>{equipList.length > 0 ? <div>{equipList}</div> : <div><h4>No Equipable Items</h4></div>}</div> : <div></div>}
       </div>
     </div>
     <button style={{ marginTop: "1%", marginBottom: "1%" }} onClick={props.Back}><h3>Leave</h3></button>
